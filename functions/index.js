ad0cda84867f362c41a88c9490fa0703e3b676a6
RAFH82 <hrechkar@gmail.com>
2020-12-31 15:27:59 -0800

    Starting to work on User API

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
-const functions = require('firebase-functions');
+// const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// // // Create and Deploy Your First Cloud Functions
+// // // https://firebase.google.com/docs/functions/write-firebase-functions
+// //
+// // exports.helloWorld = functions.https.onRequest((request, response) => {
+// //   functions.logger.info("Hello logs!", {structuredData: true});
+// //   response.send("Hello from Firebase!");
+// // });
+
+const functions = require("firebase-functions");
+const app = require("express")();
+const {
+	getAllTodos,
+	postOneTodo,
+	deleteTodo,
+	editTodo,
+} = require("./APIs/todos");
+
+app.get("/todos", getAllTodos);
+exports.api = functions.https.onRequest(app);
+
+app.post("/todo", postOneTodo);
+
+app.put("/todo/:todoId", editTodo);
+
+app.delete("/todo/:todoId", deleteTodo);
